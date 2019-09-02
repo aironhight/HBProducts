7932df90999bc756a65720eaab2a860908bb43d9
aironhight <aironhight@yahoo.com>
2019-09-02 08:29:49 +0200

    Init
    Initial commit

---
+++ HBProducts/HBProducts/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using HBProducts.Services;
+using HBProducts.Views;
+
+namespace HBProducts
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
