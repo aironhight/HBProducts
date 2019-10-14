13e1ea548591ddaf833b06a972d66b8cf7a816b8
konstantinralev <konstantin.ralev@gmail.com>
2019-10-14 16:31:41 +0200

    iOS QR Scanner - Fixed

--- HBProducts/HBProducts.iOS/AppDelegate.cs
+++ HBProducts/HBProducts.iOS/AppDelegate.cs
@@ -25,6 +25,8 @@ namespace HBProducts.iOS
             global::Xamarin.Forms.Forms.Init();
             LoadApplication(new App());
 
+            ZXing.Net.Mobile.Forms.iOS.Platform.Init();
+
             return base.FinishedLaunching(app, options);
         }
     }
