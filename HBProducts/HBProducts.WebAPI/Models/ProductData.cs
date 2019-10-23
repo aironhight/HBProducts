48b6431610c306bff657a5f917c7944c5352e9f4
Mariyan Mariyanov Deligalabov <253896@VIAUC.DK>
2019-10-23 13:11:39 +0200

    Removing the Web Api from the App Solution

---
+++ HBProducts/HBProducts.WebAPI/Models/ProductData.cs
@@ -0,0 +1,45 @@
+﻿using HBProducts.WebAPI.Models;
+using System;
+
+namespace HBProducts.Models
+{
+    [Serializable]
+    public class ProductData //: BaseViewModel
+    {
+        private String dataType;
+        private String dataValue;
+        private Boolean isUrl;
+
+        public ProductData(String dataType, String dataValue, Boolean isUrl)
+        {
+            this.dataType = dataType;
+            this.dataValue = dataValue;
+            this.isUrl = isUrl;
+        }
+
+        public String GetType()
+        {
+            return dataType;
+        }
+
+        public String GetValue()
+        {
+            return dataValue;
+        }
+
+        public Boolean IsUrl()
+        {
+            return isUrl;
+        }
+
+        public String Value
+        {
+            get { return dataValue; }
+        }
+
+        public String Type
+        {
+            get { return dataType; }
+        }
+    }
+}
---
+++ HBProducts/HBProducts.WebAPI/Models/ProductList.cs
@@ -0,0 +1,34 @@
+﻿using HBProducts.WebAPI.Models;
+using System;
+using System.Collections.ObjectModel;
+
+namespace HBProducts.Models
+{
+    [Serializable]
+    public class ProductList //: BaseViewModel
+    {
+        private ObservableCollection<Product> products = new ObservableCollection<Product>();
+
+        public ProductList()
+        {
+            products = new ObservableCollection<Product>();
+        }
+
+        public ObservableCollection<Product> Products
+        {
+            set { SetProperty(ref products, value); }
+            get { return products; }
+        }
+
+        //Methods to implement commands to manipulate the product list...
+        public void RemoveProduct(Product product)
+        {
+            Products.Remove(product);
+        }
+
+        public void AddProduct(Product product)
+        {
+            Products.Add(product);
+        }
+    }
+}
