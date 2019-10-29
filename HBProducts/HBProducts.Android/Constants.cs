3aa25609c621b6a77ded0d9cea19866e8889f66d
konstantinralev <konstantin.ralev@gmail.com>
2019-10-29 10:56:09 +0100

    CleanUp 2

--- HBProducts/HBProducts.Android/Constants.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace HBProducts.Droid
-{
-    public static class Constants
-    {
-        public const string ListenConnectionString = "<Endpoint=sb://bachproj.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=8ydPv6lUvOcBaozPOSnfyNUMQBrOqeCblKKoUoo7xmg=>";
-        public const string NotificationHubName = "<NotificationTest>";
-    }
-}
