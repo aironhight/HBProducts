07c58e160a36609236f5d6fbc680428332638f46
konstantinralev <konstantin.ralev@gmail.com>
2019-11-28 00:34:04 +0100

    Background Notification Android FIx

--- HBProducts/HBProducts.Android/MainActivity.cs
+++ HBProducts/HBProducts.Android/MainActivity.cs
@@ -12,7 +12,7 @@ using Android.Content;
 namespace HBProducts.Droid
 {
 
-    [Activity(Label = "HBProducts", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "HBProducts", Icon = "@mipmap/icon", Theme = "@style/MainTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
 
@@ -28,29 +28,23 @@ namespace HBProducts.Droid
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+            AzurePushNotificationManager.ProcessIntent(this, Intent);
 
-            //Set the default notification channel for your app when running Android Oreo
-            if (Build.VERSION.SdkInt >= Android.OS.BuildVersionCodes.O)
-            {
-                //Change for your default notification channel id here
-                AzurePushNotificationManager.DefaultNotificationChannelId = "DefaultChannel";
-
-                //Change for your default notification channel name here
-                AzurePushNotificationManager.DefaultNotificationChannelName = "General";
-            }
-
-            
-      AzurePushNotificationManager.Initialize(this, Constants.ListenConnectionString, Constants.NotificationHubName, true);
-            CrossAzurePushNotification.Current.RegisterAsync(new string[] { "all", "general" });
         }
 
-       
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
-      
-	}
+
+        protected override void OnNewIntent(Intent intent)
+        {
+            base.OnNewIntent(intent);
+            AzurePushNotificationManager.ProcessIntent(this, intent);
+        }
+
+    }
 }
